2eeca1bf5ec500d351cdd44aceedde32556757f8
red <red@test.com>
2026-05-18 12:08:57 +0300

    r0

---
+++ G.java
@@ -0,0 +1,30 @@
+public class G implements B, E {
+
+    private int i = 42;
+
+    private double e = 100.500;
+
+    public int ae() {
+        return java.lang.Math.abs(-7);
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public void bb() {
+        System.out.println(getClass().getName());
+    }
+}
---
+++ H.java
@@ -0,0 +1,30 @@
+public class H implements B, E {
+
+    private double g = 100.500;
+
+    private byte e = 1;
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public Object gg() {
+        return new java.util.Random();
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public void bb() {
+        System.out.println(42);
+    }
+}
